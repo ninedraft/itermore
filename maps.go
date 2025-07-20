b6e134037c4f251b674d57dcef3a7065dd202dd1
merlin <clamores.pro@gmail.com>
2025-07-20 14:20:58 +0300

    refactor: reorganize imports and simplify CollectMap implementation

--- maps.go
+++ maps.go
@@ -1,6 +1,9 @@
 package itermore
 
-import "iter"
+import (
+	"iter"
+	"maps"
+)
 
 // Map creates a sequence that yields key-value pairs from the given map.
 // If map nil or empty, the sequence will be empty.
@@ -17,9 +20,7 @@ func Map[K comparable, V any](m map[K]V) iter.Seq2[K, V] {
 // CollectMap writes key-value pairs from provided sequence to the given map.
 // It will panic if dst is nil.
 func CollectMap[M ~map[K]V, K comparable, V any](dst M, seq iter.Seq2[K, V]) {
-	for k, v := range seq {
-		dst[k] = v
-	}
+	maps.Insert(dst, seq)
 }
 
 // CollectKeys writes keys from provided sequence to the given map and assigns them the given value.
